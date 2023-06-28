d9eabc13b8102282a3f32c2e5da195d4e2def775
lining98 <2357490894@qq.com>
2023-06-28 16:30:29 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dataV from '@jiaminghi/data-view'
+import './assets/css/index.css'
 
 Vue.config.productionTip = false
+Vue.use(dataV);
 
 new Vue({
   router,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content bg">
+    <dv-full-screen-container>
+      <!-- 一行二等分 -->
+      <div class="module-box">
+        <div style="flex: 0 1 50%">
+          <dv-border-box-8 style="width: 100%; height: 200px">
+            <dv-capsule-chart
+              :config="config"
+              style="width: 100%; height: 200px"
+            />
+          </dv-border-box-8>
+        </div>
+        <div style="flex: 0 1 50%">
+          <dv-border-box-3 style="width: 100%; height: 200px"
+            >dv-border-box-3</dv-border-box-3
+          >
+        </div>
+      </div>
+
+      <div class="module-box">
+        <div style="flex: 0 1 25%">
+          <dv-border-box-9 style="width: 100%; height: 200px"
+            >dv-border-box-3</dv-border-box-9
+          >
+          <dv-border-box-7 style="width: 100%; height: 200px"
+            >dv-border-box-3</dv-border-box-7
+          >
+          <dv-border-box-2 style="width: 100%; height: 200px"
+            >dv-border-box-3</dv-border-box-2
+          >
+        </div>
+        <div style="flex: 0 1 50%">
+          <dv-border-box-8 style="width: 100%; height: 600px"
+            >dv-border-box-3</dv-border-box-8
+          >
+        </div>
+        <div style="flex: 0 1 25%">
+          <dv-border-box-1 style="width: 100%; height: 300px"
+            >dv-border-box-3</dv-border-box-1
+          >
+          <dv-border-box-7 style="width: 100%; height: 300px"
+            >dv-border-box-3</dv-border-box-7
+          >
+        </div>
+      </div>
+
+      <div class="module-box">
+        <div style="flex: 0 1 70%">
+          <dv-border-box-7 style="width: 100%; height: 200px"
+            >dv-border-box-3</dv-border-box-7
+          >
+        </div>
+        <div style="flex: 0 1 30%">
+          <dv-border-box-9 style="width: 100%; height: 200px"
+            >dv-border-box-3</dv-border-box-9
+          >
+        </div>
+      </div>
+    </dv-full-screen-container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+  data() {
+    return {
+      config: {
+        data: [
+          {
+            name: "南阳",
+            value: 167,
+          },
+          {
+            name: "周口",
+            value: 67,
+          },
+          {
+            name: "漯河",
+            value: 123,
+          },
+          {
+            name: "郑州",
+            value: 55,
+          },
+          {
+            name: "西峡",
+            value: 98,
+          },
+        ],
+        colors: ["#e062ae", "#fb7293", "#e690d1", "#32c5e9", "#96bfff"],
+        unit: "单位",
+        showValue:true
+      },
+    };
+  },
+};
 </script>
+
+<style scoped>
+.content {
+  background: #010d39;
+  color: #ffffff;
+  height: 100vh;
+  display: flex;
+}
+.module-box {
+  display: flex;
+  margin: 10px;
+}
+</style>
